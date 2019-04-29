f923987deeea89be6e838fad073f75c8efb7d5f5
Eason <leoling127@gmail.com>
2019-04-29 12:11:03 +0800

    Add method-override and insert data OK

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use(methodOverride('_method'))
 
 // mongoose 連線後透過 mongoose.connection 拿到 Connection 的物件
 mongoose.connect('mongodb://127.0.0.1/expensetracker', { useNewUrlParser: true, useFindAndModify: false })
--- routes/record.js
+++ routes/record.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const Record = require('../models/record')
 
 router.get('/new', (req, res) => {
   res.render('new')
 })
 
+router.post('/new', (req, res) => {
+  const newRecord = new Record(req.body)
+  newRecord.save().then(() => {
+    res.redirect('/')
+  })
+})
+
 router.get('/edit/:id', (req, res) => {
   res.render('edit')
+})
+
+router.put('/edit/:id', (req, res) => {
+  res.send('edit')
+})
+
+router.delete('/delete/:id', (req, res) => {
+  res.send('delete')
 })
 
 module.exports = router
